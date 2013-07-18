3cf491f458d9be49453fc44f53aa7b51acb43268
Archit Baweja <archit@do.com>
2013-07-17 18:12:05 -0700

    Initial XCode commit.

---
+++ Koel/ABKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABKAppDelegate.h
+//  Koel
+//
+//  Created by Archit Baweja on 7/17/13.
+//  Copyright (c) 2013 Archit Baweja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Koel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Koel
+//
+//  Created by Archit Baweja on 7/17/13.
+//  Copyright (c) 2013 Archit Baweja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABKAppDelegate class]));
+    }
+}
